274bd42557dbb7a70c178360c017f3d46774af69
Denys Potapov <denys.potapov@gmail.com>
2025-04-01 18:58:15 +0300

    more unit tests

--- remelange.js
+++ remelange.js
@@ -11,7 +11,9 @@ function remelange(jsCode) {
   const fs = require("fs");
   const tree = parser.parse(jsCode);
 
-  return "(* COMPILED WITH remelange *)\n" + nodeToOCaml(tree.rootNode);
+  return removeEmptyLines(
+    "(* COMPILED WITH remelange *)\n" + nodeToOCaml(tree.rootNode),
+  );
 }
 
 function nodeToOCaml(node) {
@@ -26,11 +28,35 @@ function nodeToOCaml(node) {
     case "statement_block":
       return node.namedChildren.map(nodeToOCaml).join("\n");
 
+    case "lexical_declaration":
+    case "expression_statement":
+      return nodeToOCaml(node.namedChildren[0]);
+
+    case "variable_declarator":
+      const name = node.namedChildren[0].text;
+      const value = nodeToOCaml(node.namedChildren[1]);
+      if (value) {
+        return `let ${name} = ${value} in`;
+      }
+
+      return "";
+
+    case "assignment_expression":
+      if (node.leftNode.text === "module.exports") {
+        return nodeToOCaml(node.rightNode);
+      }
+      return "";
+
+    case "parenthesized_expression":
+      const inner = nodeToOCaml(node.namedChildren[0]);
+      return inner ? `(${inner})` : "";
+
     case "comment":
       return `(*${node.text}*)`;
 
+    case "function_expression":
     case "function_declaration": {
-      const fnName = node.namedChildren[0].text;
+      const fnName = toSnakeCase(node.namedChildren[0].text);
       const params = node.namedChildren[1].namedChildren
         .map((p) => p.text)
         .join(" ");
@@ -42,14 +68,18 @@ function nodeToOCaml(node) {
       return `let ${rec}${fnName} ${params} =\n${body}`;
     }
 
+    case "if_statement":
+      const cond = nodeToOCaml(node.namedChildren[0]);
+      if (cond) {
+        const _then = nodeToOCaml(node.namedChildren[1]);
+        const _else = nodeToOCaml(node.namedChildren[2]);
+
+        return `if ${cond} then ${_then}${_else}`;
+      }
+      return "";
+
     case "else_clause":
       return `\nelse ${nodeToOCaml(node.namedChildren[0])}`;
-
-    case "if_statement":
-      const cond = node.namedChildren[0].text;
-      const _then = nodeToOCaml(node.namedChildren[1]);
-      const _else = nodeToOCaml(node.namedChildren[2]);
-      return `if ${cond} then ${_then}${_else}`;
 
     case "number":
     case "identifier":
@@ -57,15 +87,27 @@ function nodeToOCaml(node) {
 
     case "binary_expression": {
       const left = nodeToOCaml(node.leftNode);
-      const operator = node.children[1].text;
+      let operator = node.children[1].text;
+      if (operator === "===") {
+        operator = "=";
+      }
+      if (operator === "%") {
+        operator = "mod";
+      }
       const right = nodeToOCaml(node.rightNode);
+
       return `${left} ${operator} ${right}`;
     }
 
     case "call_expression": {
-      const fnName = node.functionNode.text; // TODO convertsnake
-      const args = node.argumentsNode.namedChildren.map(nodeToOCaml).join(" ");
-      return `(${fnName} ${args})`;
+      const fnName = convertCall(node.functionNode.text);
+      if (fnName) {
+        const args = node.argumentsNode.namedChildren
+          .map(nodeToOCaml)
+          .join(" ");
+        return `(${fnName} ${args})`;
+      }
+      return "";
     }
 
     case "return_statement": {
@@ -73,8 +115,36 @@ function nodeToOCaml(node) {
     }
 
     default:
-      console.warn(`Unsupported node type ${node.type}`);
+      console.warn(`Unsupported node type ${node.type} at ${node.text}`);
       return "";
+  }
+}
+
+function toSnakeCase(name) {
+  return name.replace(/([a-z])([A-Z])/g, "$1_$2").toLowerCase();
+}
+
+function removeEmptyLines(code) {
+  return code
+    .split("\n")
+    .filter((line) => line.trim() !== "")
+    .join("\n");
+}
+
+function convertCall(fnName) {
+  switch (fnName) {
+    case "require":
+    case "isNumber":
+    case "Number.isInteger":
+    case "Number.isSafeInteger":
+      return "";
+
+    case "Math.abs":
+      // TODO add float support
+      return "Int.abs";
+
+    default:
+      return fnName;
   }
 }
 
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const remelange = require("./remelange");
 
-test("Simple test", () => {
+test("Fibonacci", () => {
   expect(
     remelange(`
 /*
@@ -23,6 +23,36 @@ function fib(n) {
 let rec fib n =
 if (n <= 1) then n
 else (fib n - 1) + (fib n - 2)
+`.trim(),
+  );
+});
+
+test("is-odd", () => {
+  expect(
+    remelange(`
+const isNumber = require('is-number');
+
+module.exports = function isOdd(value) {
+    const n = Math.abs(value);
+    if (!isNumber(n)) {
+        throw new TypeError('expected a number');
+    }
+    if (!Number.isInteger(n)) {
+        throw new Error('expected an integer');
+    }
+    if (!Number.isSafeInteger(n)) {
+        throw new Error('value exceeds maximum safe integer');
+    }
+
+    return (n % 2) === 1;
+};
+`).trim(),
+  ).toBe(
+    `
+(* COMPILED WITH remelange *)
+let is_odd value =
+let n = (Int.abs value) in
+(n mod 2) = 1
 `.trim(),
   );
 });
